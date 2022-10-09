cf78b197a421be83f0eece71bf2e8598d16b44f7
jan <jankompatscher@gmail.com>
2022-10-09 12:28:23 +0300

    react-navbar

--- src/App.js
+++ src/App.js
@@ -1,41 +1,22 @@
-import React, {useState, useEffect } from 'react';
-import {TextField , Button } from '@mui/material';
-import Todo from './components/Todo';
-import { db } from './firebase.js';
-import { collection , query, orderBy , onSnapshot, addDoc,serverTimestamp} from 'firebase/firestore';
-import './App.css';
-const q=query(collection(db,'todos'),orderBy('timestamp','desc'));
+import Navbar from "./Navbar"
+import Pricing from "./pages/Pricing"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import { Route, Routes } from "react-router-dom"
+
 function App() {
-const [todos,setTodos]=useState([]);
-const [input, setInput]=useState('');
-useEffect(() => {
-onSnapshot(q,(snapshot)=>{
-setTodos(snapshot.docs.map(doc=>({
-id: doc.id,
-item: doc.data()
-})))
-})
-},[input]);
-const addTodo=(e)=>{
-e.preventDefault();
-addDoc(collection(db,'todos'),{
-todo:input,
-timestamp: serverTimestamp()
-})
-setInput('')
-};
-return (
-<div className="App">
-<h2> TODO List App</h2>
-<form>
-<TextField id="outlined-basic" label="Make Todo" variant="outlined" style={{margin:"0px 5px"}} size="small" value={input}
-onChange={e=>setInput(e.target.value)} />
-<Button variant="contained" color="primary" onClick={addTodo}  >Add Todo</Button>
-</form>
-<ul>
-{todos.map(item=> <Todo key={item.id} arr={item} />)}
-</ul>
-</div>
-);
+  return (
+    <>
+      <Navbar />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/pricing" element={<Pricing />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </>
+  )
 }
-export default App;
+
+export default App
