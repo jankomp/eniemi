da6333e7a4f93ced50587627e69a535a2c3079fc
AlleyXueqi <alleyalley@yeah.net>
2022-11-14 16:14:46 +0200

    Layout of Login/Profile

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,4 +1,5 @@
 import { getAuth } from "firebase/auth"
+import { Button} from "@mui/material"
 
 export default function Profile() {
 const auth = getAuth();
@@ -7,5 +8,9 @@ const user = auth.currentUser;
   return (<>
     <h1>{user.displayName}</h1>
     <p>{user.email}</p>
+    <div>
+       <Button variant="contained">Contained</Button>
+       <Button variant="outlined">Outlined</Button>
+    </div>
     </>)
 }
