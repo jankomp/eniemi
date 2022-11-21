cef1594dbad1a26c2517dc6f39e248dc6c71d321
AlleyXueqi <alleyalley@yeah.net>
2022-11-21 13:45:43 +0200

    Filter fixed

--- src/components/OfferListFilter.js
+++ src/components/OfferListFilter.js
@@ -35,33 +35,31 @@ export default function OfferListFilter({returnFilter}) {
     }
 
     return (<>
-        <form onSubmit={event => returnFilter(event, filter)}>
-            <div className="index-inputbox">
-                <div className="index-in-left">
-                    <img src="http://celiang.oss-cn-hangzhou.aliyuncs.com/measurement/2022-11/20/78ZBZphpHfMTnS1668876808221120.png" />
-                </div>
-                <div className="index-in-right">
-                <CategoryDropDown valueSelected={CategorySelected}/>
-                </div>
-            
-            </div>
-             
-             {/* <label>
-            Category:
-            </label>
-            <CategoryDropDown valueSelected={CategorySelected}/>
-            <br />
-            <label >
-            Maximum price:
-            </label>
-            <input 
-                name="maxPrice"
-                type="number"
-                value={filter.maxPrice}
-                onChange={HandleInputChange} />
-            <input type="submit" value="Apply Filter"/> */}
-        </form>
-        {/* <button onClick={localClearFilter}>Clear filter</button> */}
-        </>
-    )
-}
+                <form onSubmit={event => returnFilter(event, filter)}>
+                    <div className="index-inputbox">
+                        {/*<div className="index-in-left">
+                            <img src="http://celiang.oss-cn-hangzhou.aliyuncs.com/measurement/2022-11/20/78ZBZphpHfMTnS1668876808221120.png" />
+                        </div>*/}
+                        <div className="index-in-right">
+                        
+                        <CategoryDropDown valueSelected={CategorySelected}/>
+                        </div>
+                    </div>
+                     
+                    <label>
+                    Maximum price:
+                    </label>
+                    <input 
+                        name="maxPrice"
+                        className="maxPrice"
+                        type="number"
+                        value={filter.maxPrice}
+                        onChange={HandleInputChange} />
+                    <br/>
+                    <div className="clearFilter" onClick={localClearFilter}>Clear filter</div>
+                    <input className="applyFilter" type="submit" value="Apply Filter"/> 
+                </form>
+                
+                </>
+            )
+        }s
